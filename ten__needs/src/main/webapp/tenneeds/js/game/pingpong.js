61cc4062265605d9e8061b7237b4533368cacfe8
Ga0Kwon <kgayeong30@gmail.com>
2023-03-29 22:14:33 +0900

    2023-03-29 네트 기준이상으로 못넘어가도록 설정, 스페이스바 누를 시 공튀기고, 스페이스바시 상태변경

--- ten__needs/src/main/webapp/tenneeds/js/game/pingpong.js
+++ ten__needs/src/main/webapp/tenneeds/js/game/pingpong.js
@@ -38,11 +38,12 @@ const ctx = canvas.getContext('2d');
 		color : "white",
 	}
 	
-	// -------------------------------------------------------- 방향키 전역 변수
+	// 방향키 전역 변수
 	let rightPressed = false;	// 우키 상태
 	let leftPressed = false;	// 좌키 상태
 	let upPressed = false;		// 상키 상태
 	let downPressed = false;	// 하키 상태
+	let spacePressed = false; //스페이스여부
 	// 선언 이유: 아래 방향키 작동 메소드로 만들어 사용하고자 함
 	
 	// 함수 정의
@@ -77,15 +78,18 @@ const ctx = canvas.getContext('2d');
 	// 반복으로 움직이게 보이는 함수
 	function render(){
 		if (rightPressed && user1.x < canvas.width - user1.width) {
-	      	user1.x += 7;
+	      	user1.x += 8;
 	    } else if (leftPressed && user1.x > 0) {
-	      	user1.x -= 7;
+	      	user1.x -= 8;
 	    }
 	    
 	    if ( upPressed && user1.y > 0 ){
-		  	user1.y -= 7;
+		  	user1.y -= 8;
 		} else if ( downPressed && user1.y < canvas.height - user1.height ) {
-		  	user1.y += 7;
+		  	if(user1.y < canvas.height/2 - 20){ //상대편 네트보다 더 아래로 갈 수 없도록
+				user1.y += 8;
+			}
+			
 		}
 	    
 	    
@@ -115,7 +119,10 @@ const ctx = canvas.getContext('2d');
 	    upPressed = true;
 	  } else if (event.key === "Down" || event.key === "ArrowDown") {
 	    downPressed = true;
-	  }
+	  } else if (event.key === "" || event.keyCode === 32) {
+		user1.color = "red";
+	    spacePressed = true;
+	  } 
   }
 
 	function keyUpHandler(event) {
@@ -127,7 +134,10 @@ const ctx = canvas.getContext('2d');
 	    upPressed = false;
 	  } else if (event.key === "Down" || event.key === "ArrowDown") {
 	    downPressed = false;
-	  }
+	  } else if (event.key === "" || event.keyCode === 32) {
+		//3초 후에 spacePressed = false로 적용
+	    setTimeout(() => spacePressed = false, user1.color = "white", 3000); //정확함도를 낮추기 위해서
+	  } 
   }
 
 	function collision(b, p){
@@ -170,30 +180,34 @@ const ctx = canvas.getContext('2d');
 		  // 패들이 사용자 또는 com 패들을 쳤는지 확인합니다.
 		let player = (ball.y < canvas.height/2) ? user1 : user2;
 		
-		 // 공이 패들에 부딪힌 경우
-		if(collision(ball, player)){
-			
-			// 공이 패들에 닿는 위치를 확인합니다.
-			let collidePoint = ball.y - (player.y + player.height/2);
-			
-			// collidePoint의 값을 정규화합니다. -1과 1 사이의 숫자를 가져와야 합니다.
-       		// -player.height/2 < 충돌 지점 < player.height/2
-			collidePoint = collidePoint/(player.height/2);
-			
-			// 공이 패들의 상단에 닿을 때 공이 -18도 각도를 가지기를 원합니다.
-    	    // 공이 패들의 중앙에 닿을 때 공이 0도 각도를 가지기를 원합니다.
-   		    // 공이 패들 바닥에 닿을 때 공이 18도 기울기를 원합니다.
-       		// Math.PI/10 = 18도
-			let angleRad = collidePoint * Math.PI/10;
-			
-			// X 및 Y 속도 방향 변경
-			let direction = (ball.x < canvas.width/2)? 1 : -1;
-			
-			ball.velocityX = direction * ball.speed * Math.cos(angleRad);
-			ball.velocityY = ball.speed * Math.sin(angleRad);
-			
-		 // 패들이 공을 칠 때마다 공의 속도를 높입니다.
-			ball.speed += 0.5;
+		
+		if(spacePressed){
+			 // 공이 패들에 부딪힌 경우
+			if(collision(ball, player)){
+				
+				// 공이 패들에 닿는 위치를 확인합니다.
+				let collidePoint = ball.y - (player.y + player.height/2);
+				
+				// collidePoint의 값을 정규화합니다. -1과 1 사이의 숫자를 가져와야 합니다.
+	       		// -player.height/2 < 충돌 지점 < player.height/2
+				collidePoint = collidePoint/(player.height/2);
+				
+				// 공이 패들의 상단에 닿을 때 공이 -18도 각도를 가지기를 원합니다.
+	    	    // 공이 패들의 중앙에 닿을 때 공이 0도 각도를 가지기를 원합니다.
+	   		    // 공이 패들 바닥에 닿을 때 공이 18도 기울기를 원합니다.
+	       		// Math.PI/10 = 18도
+				let angleRad = collidePoint * Math.PI/10;
+				
+				// X 및 Y 속도 방향 변경
+				let direction = (ball.x < canvas.width/2)? 1 : -1;
+				
+				ball.velocityX = direction * ball.speed * Math.cos(angleRad);
+				ball.velocityY = ball.speed * Math.sin(angleRad);
+				
+			 // 패들이 공을 칠 때마다 공의 속도를 높입니다.
+				ball.speed += 0.5;
+			}
+		
 		}
 		
 		
---
+++ ten__needs/src/main/webapp/test/testing/testing.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function(event) {
+
+    console.log(event.key);
+
+    console.log(event.keyCode);
+
+});
