2a9d4fbef28de5bb41c5e91a8552455f88195f30
gayeongkwon <gayeongkwon@localhost>
2023-03-31 14:15:13 +0900

    2023-03-31 14:15 게임결과 dto 생

--- ten__needs/src/main/java/model/dao/GameDao.java
+++ ten__needs/src/main/java/model/dao/GameDao.java
@@ -1,5 +1,7 @@
 package model.dao;
 
+import model.dto.GameResultDto;
+
 public class GameDao extends Dao{
 	/*싱글톤*/
 	private static GameDao gameDao = new GameDao();
@@ -7,19 +9,21 @@ public class GameDao extends Dao{
 	public static GameDao getInstance() {return gameDao;}
 	
 	//게임 승리시 게임 종료 -> 데이터 넣기(승리자)
-	public boolean endGame(int winner) {
-		String sql = "update into game set winner = ";
+	public boolean endGame(GameResultDto dto) {
+		String sql = "update into gamestatus set gResult = 1, gsAccute = ? where mNo = " + dto.getWinnerMno();
 		
 		try {
 			ps = con.prepareStatement(sql);
 			
-			ps.setInt(1, winner);
+			ps.setDouble(1, dto.getWinnerAccute());
 			
 			int count =  ps.executeUpdate();
 			
 			if(count == 1) {
 				//게임 승리자의 gResult의 값을 1로 바꿔준다.
-				sql = "update into gamestatus set gResult = 1 where mNo = " + winner;
+				sql = "update into gamestatus set gResult = 1, gsAccute = ? where mNo = " + dto.getLoserMno();
+				
+				ps.setDouble(1, dto.getLoserAccute());
 				
 				ps.executeUpdate();
 				
