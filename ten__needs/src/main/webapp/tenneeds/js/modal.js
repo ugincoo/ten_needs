ddee2c2caae3e73583b50e9b43a0da15376fa77a
Ga0Kwon <Kgayeong39@gmail.com>
2023-04-11 17:29:14 +0900

    2023/04/11 17:29

--- ten__needs/src/main/webapp/tenneeds/js/modal.js
+++ ten__needs/src/main/webapp/tenneeds/js/modal.js
@@ -4,13 +4,16 @@ function openModal(){
 }
 
 function closeModal(){
-	if(document.querySelector('.roundContent').innerHTML == "최종 승리!"){
-		location.href = "/ten__needs/tenneeds/jsp/game/gamelist.jsp";
-	}
 	document.querySelector('.modal_wrap').style.display = 'none';
 }
 
 
 function closeModal2(){
 	document.querySelector('.modal_wrap2').style.display = 'none';
+}
+
+function closeModal3(){
+	if(document.querySelector('.roundContent').innerHTML == "최종 승리!"){
+		location.href = "/ten__needs/tenneeds/jsp/game/gamelist.jsp";
+	}
 }
